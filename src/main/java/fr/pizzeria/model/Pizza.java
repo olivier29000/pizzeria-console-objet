de5ae93a2e25585cb656ac0e54bace25f8e3bded
Olivier LASBLEIS <lasbleis.olivier@yahoo.fr>
2019-04-30 17:06:48 +0200

    TP11.1.bis

--- src/main/java/fr/pizzeria/model/Pizza.java
+++ src/main/java/fr/pizzeria/model/Pizza.java
@@ -2,4 +2,25 @@ package fr.pizzeria.model;
 
 public class Pizza {
 
+	private static int id = 0;
+	private String code;
+	private String libelle;
+	private double prix;
+
+	/**
+	 * Constructeur de la classe pizza
+	 * 
+	 * @param id
+	 * @param code
+	 * @param libelle
+	 * @param prix
+	 */
+	public Pizza(String code, String libelle, double prix) {
+
+		id++;
+		this.code = code;
+		this.libelle = libelle;
+		this.prix = prix;
+	}
+
 }
